a3a02a9e5cba7a4501739708ec70602070da1c56
Adrian C <adek2222@hotmail.com>
2020-03-17 19:43:23 +0100

    add two views about and home

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,21 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Router from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component() {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue');
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
     },
-  },
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+  ],
 });
-
-export default router;
